7b6739779d82694ac6f179f788eacd0e3e0de61b
Tantely Rahobisoa <143869752+trahobisoa@users.noreply.github.com>
2023-10-11 17:02:16 +0800

    Add files via upload

---
+++ TP Immeubles/sol.py
@@ -0,0 +1,19 @@
+
+import turtle
+from trait import trait
+
+# ----- Sol de la rue -----
+def sol(y_sol):
+    '''
+    ParamÃ¨tres
+        y_sol : ordonnÃ©e du sol du la rue
+    Cete fonction dessine un trait horizontale de 3 pixels d'Ã©paisseur
+    '''
+
+    pass
+
+
+if __name__ == '__main__':
+    sol(0)
+    # On ferme la fenÃªtre s'il y a un clique gauche
+    turtle.exitonclick()
